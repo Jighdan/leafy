5b2061e1a3724df9214b9005c5eccd97c7fd0d56
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2024-10-14 12:00:54 +0100

    style: animating sidebar

--- app/src/components/DirectoryNavigation/components/Item.tsx
+++ app/src/components/DirectoryNavigation/components/Item.tsx
@@ -1,6 +1,10 @@
+"use client";
+
+import { useState, useEffect, useMemo, useRef } from 'react';
 import { Directory } from "~/services/api/modules";
 import { ItemIcon } from "./ItemIcon";
 import Link from "next/link";
+import { usePathname } from 'next/navigation';
 import cx from "classnames";
 
 interface Props {
@@ -9,23 +13,59 @@ interface Props {
 }
 
 export function Item({ directory, depth }: Props) {
-  const href = `/editor/${directory.path}`;
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+  const href = useMemo(() => `/editor/${directory.path}`, [directory.path]);
+  const [height, setHeight] = useState<number | undefined>(0);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (pathname.startsWith(href)) {
+      setIsOpen(true);
+    }
+  }, [pathname, href]);
+
+  useEffect(() => {
+    if (!height || !isOpen || !ref.current) return undefined;
+    
+    const timeout = setTimeout(() => {
+      setHeight(undefined);
+    }, 300);
+
+    return () => clearTimeout(timeout);
+  }, [height, isOpen]);
+
+  useEffect(() => {
+    if (isOpen) {
+      const height = ref.current?.getBoundingClientRect().height;
+      setHeight(height);
+    } else {
+      setHeight(0);
+    }
+  }, [isOpen]);
+
+  const toggleOpen = (e: React.MouseEvent) => {
+    if (directory.item_type === "directory" && directory.children?.length > 0) {
+      e.preventDefault();
+      setIsOpen(!isOpen);
+    }
+  };
 
   return (
-    <div className="block">
+    <div key={directory.path} className="block group">
       <Link
         href={href}
         shallow
         className="flex items-center gap-2 cursor-pointer"
+        onClick={toggleOpen}
       >
         <ItemIcon
           item_type={directory.item_type}
           className={cx({
-            "transition-transform duration-300 rotate-90":
-              directory.item_type === "directory",
+            "transition-transform duration-300": directory.item_type === "directory",
+            "rotate-90": isOpen && directory.item_type === "directory",
           })}
         />
-
         <span className="line-clamp-1 text-sm">
           {directory.path.split("/").pop()}
         </span>
@@ -33,10 +73,15 @@ export function Item({ directory, depth }: Props) {
 
       {directory.item_type === "directory" &&
         directory.children?.length > 0 && (
-          <div className="block ml-2 pl-2 border-l border-l-black/50">
-            {directory.children.map((child, index) => (
-              <Item key={index} directory={child} depth={depth + 1} />
-            ))}
+          <div 
+            className="ml-2 pl-2 border-l border-l-black/50 overflow-hidden transition-[height] duration-300 ease-in-out"
+            style={{ height: height === undefined ? 'auto' : `${height}px` }}
+          >
+            <div ref={ref}>
+              {directory.children.map((child, index) => (
+                <Item key={index} directory={child} depth={depth + 1} />
+              ))}
+            </div>
           </div>
         )}
     </div>
